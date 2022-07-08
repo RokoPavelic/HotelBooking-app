c234457887048354acc48a8a3d3e0b78d34ffcd6
Antonello Fois <antonello.fois1381@gmail.com>
2022-07-08 11:57:28 +0200

    first commit

---
+++ app/Http/Controllers/FinalProjectController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FinalProjectController extends Controller
+{
+    public function index()
+    {
+        return view('final-project');
+    } 
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel/React</title>
+    </head>
+    <body class="antialiased">
+        
+        <div id="root"></div>
+        <script src="{{ mix('js/finalProject.js') }}"></script>
+    </body>
+</html>
