24718713244b68fb9b7a91017c34b56cc2a454b9
Antonello Fois <antonello.fois1381@gmail.com>
2022-07-10 14:12:06 +0200

    footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,21 @@
+
+<footer class="footer">
+    <div className="footer-address">
+        <h3>Chateau Trebesice</h3>
+        <h5>ADDRESS:</h5>
+        <ul>
+            <li>Zámek</li>
+            <li>Třebešice</li>
+            <li>28601 Čáslav</li>
+            <li>Czech Republic</li>
+        </ul>
+    </div>
+    <div className="footer-social">
+        <ul>
+            <li><a href="#">FACEBOOK</a></li>
+            <li><a href="#">INSTAGRAM</a></li>
+            <li><a href="#">TWITTER</a></li>
+            <li><a href="#">SNAPCHAT</a></li>
+        </ul>
+    </div>
+</footer>
