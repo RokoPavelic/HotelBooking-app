4c49560152f4f2f2517bb9e2f9e4dc0ced13b3ab
Marian <marsaldana@Marians-MacBook-Air.local>
2022-07-11 20:25:44 +0200

    home page update

--- resources/js/final-project/components/pages/Home.js
+++ resources/js/final-project/components/pages/Home.js
@@ -1,7 +1,14 @@
-
+import React from   'react'
 
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <div className='main_home'>
+        <img className='homeHero' src="./images/img/main_hero.jpeg" alt='chateau' />
+            <h1>A Memorable Experience.</h1>     
+               <button className='btn'> Reserve Now</button>
+        </div>
     )
 }
+
+
+
