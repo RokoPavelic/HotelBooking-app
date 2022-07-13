f9ea8e3bb8ecf156ec138460c04d7fbbe1f8bd39
Marian <marsaldana@Marians-MacBook-Air.local>
2022-07-13 13:05:34 +0200

    events page

--- resources/js/final-project/components/pages/Events.js
+++ resources/js/final-project/components/pages/Events.js
@@ -4,7 +4,7 @@ export default function Events() {
             <div className="events-hero">
                 <h1 className="events__title">A Memorable Experience.</h1>
             </div>
-            <div className="events-content">
+            <div className="content">
                 <h3> Let us host you events</h3>
                 <p>
                     We count with experitse staff to make your event one in a
@@ -13,7 +13,7 @@ export default function Events() {
                     all-involving sensory experiance for your guests
                 </p>
             </div>
-            <div className="events-gallery">
+            <div className="gallery_cont">
                 <img
                     className="gallery_img"
                     src="./images/events_gallery_garden.jpeg"
@@ -28,10 +28,36 @@ export default function Events() {
                 />
                 <img
                     className="gallery_img"
-                    src="./images/events_gallery_toast.jpeg"
+                    src="./images/events_gallery_gathering.jpeg"
                     width="300px"
                     height="300px"
                 />
+            </div>
+            <button className="events-btn">Book Consultation</button>
+
+            <div className="services">
+                <div className="services-container">
+                    <h3 className="services-title"> Our Services Include:</h3>
+                    <ul>
+                        <li>Catering</li>
+                        <li>Professiona Chef</li>
+                        <li>Accommodation for guests</li>
+                    </ul>
+                </div>
+                <div className="services-container">
+                    <ul>
+                        <li>Seating- staging</li>
+                        <li>Custom Decoration</li>
+                        <li>Professional Photographer</li>
+                    </ul>
+                </div>
+                <div className="services-container">
+                    <ul>
+                        <li>Florist and Baker</li>
+                        <li>Professional Organizer</li>
+                        <li>Hosting services</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
