f7e8483ad82a1340fff28d7948e0bfbdbaebea96
Marian <marsaldana@Marians-MacBook-Air.local>
2022-07-14 14:44:58 +0200

    events fix

--- resources/js/final-project/components/pages/Events.js
+++ resources/js/final-project/components/pages/Events.js
@@ -1,62 +1,47 @@
-import { useNavigate } from "react-router-dom";
-
 export default function Events() {
-    const navigate = useNavigate();
-
     return (
         <div className="events">
             <div className="events-hero">
-                {/* KEEP AS H1 */}
-                <h1> A Memorable Experiance</h1>{" "}
-                 {/* KEEP AS H1 */}
+                <h1 className="events__title">A Memorable Experience.</h1>
             </div>
-            <h3 className="about_title">
-                        Let us host you event
-                    </h3>
             <div className="content">
+                <h3> Let us host your events</h3>
                 <p>
-                    We count with experianced staff to make your event a one in
-                    a lifetime experiance. Let our enchanting atmospher be the
-                    setting for your special day,
-                    <br />
-                    let our award winning chef add an all-involving sensory
-                    experiance for your guests.
+                    We count with experienced staff to make your event a one in a
+                    lifetime experience.
+                     Let our enchanting atmosphere be the setting
+                    for your special day,<br/>
+                     let our award winning chef add an
+                    all-involving sensory experience for your guests.
                 </p>
             </div>
             <div className="gallery_cont">
                 <img
                     className="gallery_img"
                     src="./images/events_gallery_garden.jpeg"
-                    width="400px"
+                    width="300px"
                     height="300px"
                 />
                 <img
                     className="gallery_img"
                     src="./images/events_gallery_gathering.jpeg"
-                    width="400px"
+                    width="300px"
                     height="300px"
                 />
                 <img
                     className="gallery_img"
                     src="./images/events_hero.jpeg"
-                    width="400px"
+                    width="300px"
                     height="300px"
                 />
             </div>
-         
-
-            <button
-                onClick={() => navigate(`/eventbook`)}
-                className="events-btn"
-            >
-                Book Consultation
-            </button>
-
+            <button className="events-btn">Book Consultation</button>
+  
             <div className="services">
             <h3 className="services-title"> Our Services Include:</h3>
-            
+           
                 <div className="services-container">
-                    
+                   
                     <ul>
                         <li>Catering</li>
                         <li>Professional Chef</li>
@@ -80,4 +65,7 @@ export default function Events() {
             </div>
         </div>
     );
-}
+ }
+  
+ 
+ 
