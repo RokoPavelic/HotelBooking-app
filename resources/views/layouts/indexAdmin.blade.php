0fe47f89cbfcef1c3eaf9f3cc91bec15b2cab00d
Antonello Fois <antonello.fois1381@gmail.com>
2022-07-18 11:30:28 +0200

    cannot open admin page without login

--- resources/views/layouts/indexAdmin.blade.php
+++ resources/views/layouts/indexAdmin.blade.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href={{ mix("css/adminStyles.css") }}>
-
+    <title>Admin Page</title>
+    @if(!Session::has('adminData'))
+    <script type="text/javascript">
+        window.location.href="{{url( 'admin/login' )}}";
+    </script>
+@endif
 </head>
 
 
