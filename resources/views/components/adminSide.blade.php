1b46d23e08c5738385622ab2b065d3ce96dd39af
Katerina Sevcikova <katerinasevcikova@hotmail.cz>
2022-07-19 19:07:44 +0200

    sidebar admin route changed

--- resources/views/components/adminSide.blade.php
+++ resources/views/components/adminSide.blade.php
@@ -6,7 +6,7 @@
             <a href="/admin">Dashboard</a>
             <a href="/adminbookings">Bookings</a>
             <a href="/admin/rooms">Rooms</a>
-            <a href="/adminevents">Event Planner</a>
+            <a href="/admin/events">Event Planner</a>
         
     </div>
 </div>
