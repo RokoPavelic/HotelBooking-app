801276034c1a6a758a5cc20b6d336db8cd319c8e
Marian <marsaldana@Marians-MacBook-Air.local>
2022-07-19 19:20:16 +0200

    lol2

--- database/seeders/EventTableSeeder.php
+++ database/seeders/EventTableSeeder.php
@@ -18,19 +18,23 @@ class EventTableSeeder extends Seeder
         DB::table('events')->insert([
 
             [
-                'title' => 'Meeting',
-                'start' => '2022-07-20 20:00:00',
-                'end' => '2022-07-21 20:00:00' ,
-                'color' =>'red',
-                'description'=> 'Summer wedding '
+                'booking_id'=> '0',
+                'room_id'=> '4',
+                'event_date'=> '2022-07-20 ',
+                'event_name' => 'Meeting',
+                'event_start' => '2022-07-20 20:00:00',
+                'event_end' => '2022-07-21 20:00:00' ,
+                'event_description'=> 'Summer wedding '
             ],
             [
-                'title' => 'Book club',
-                'start' => '2022-07-23 20:00:00',
-                'end' => '2022-07-24 20:00:00' ,
-                'color' =>'blue',
-                'description'=> 'Summer wedding '
-            ]
+                'booking_id'=> '1',
+                'room_id'=> '6',
+                'event_date'=> '2022-07-24',
+                'event_name' => 'wedding',
+                'event_start' => '2022-07-24 20:00:00',
+                'event_end' => '2022-07-24 23:00:00' ,
+                'event_description'=> 'nigh wedding '
+            ],
             ]);
     }
 }
