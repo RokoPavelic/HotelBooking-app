883acf2fd06279240e9b5e5c902ac68a988ea1fa
Marian <marsaldana@Marians-MacBook-Air.local>
2022-07-20 14:20:21 +0200

    lol

--- resources/views/components/adminSide.blade.php
+++ resources/views/components/adminSide.blade.php
@@ -4,7 +4,7 @@
     <div class="sidebar-links">
         
             <a href="/admin">Dashboard</a>
-            <a href="/adminbookings">Bookings</a>
+            {{-- <a href="/adminbookings">Bookings</a> --}}
             <a href="/admin/rooms">Rooms</a>
             <a href="/admin/events">Event Planner</a>
         
